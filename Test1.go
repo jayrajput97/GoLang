bbc2ebf43d2b5d9342f9f2877e871f0a73d20425
Jay Rajput <jayrajputd@gmail.com>
2021-12-31 00:07:09 -0500

    init

---
+++ Test1.go
@@ -0,0 +1,9 @@
+package main //Package name is needed. Same as importing functions, third party packages can be used in your code too
+
+import (
+	"fmt" //the brackets are not required if you are importing just one library
+)
+
+func main() {
+    fmt.Println("Hello World!")	//Simple Print statement
+}
