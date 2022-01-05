246c167f2a4f5e63089af16c68b50cd7ca3a18b9
Jay Rajput <jayrajputd@gmail.com>
2022-01-04 21:04:33 -0500

    resolved scan

--- Calc.go
+++ Calc.go
@@ -3,47 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	var choice , num1, num2 int 
+	var num1, num2 float64 
+	var choice string
 	fmt.Println("Enter two numbers")
 	num1, num2 = scanNumber()
-	fmt.Println("Enter your choice\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Mod\nAny other key to exit...")
-	fmt.Scanf("%d", &choice)
+	fmt.Printf("Enter your choice\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Mod\nAny other key to exit...")
+	fmt.Scanf("%s", &choice)
 	result := calcResult(num1, num2, choice)
-	fmt.Println("Calculation result: ", result)
+	fmt.Printf("Calculation result: %.2f", result)
 }
 
-func scanNumber() (int, int) { 
-	var x, y int
-	//fmt.Println("Enter 1st number")
-	fmt.Scanf("%f %f", &x, &y)
-	//fmt.Println("Enter 2nd number")
-	//fmt.Scanf("%f", &y)
+
+func scanNumber() (float64, float64) {
+	var x, y float64
+	fmt.Println("Enter 1st number")
+	fmt.Scanf("%f\n", &x)
+	fmt.Println("Enter 2nd number")
+	fmt.Scanf("%f\n", &y)
 	return x, y
 }
 
-func calcResult(a, b int, z int) float64 {
+func calcResult(a, b float64, z string) float64 {
 	var result float64
 	switch z {
-	case 1: //Case 1 would be Addition
+	case "1": //Case 1 would be Addition
 			result = float64(a + b)  
 			break
 
-	case 2: //Case 2 would be Subtraction
+	case "2": //Case 2 would be Subtraction
 			result = float64(a - b)
 			break
 
-	case 3: //Case 3 would be Multiplication
+	case "3": //Case 3 would be Multiplication
 			result = float64(a * b)
 			break
 
-	case 4: //Case 4 would be Divide
+	case "4": //Case 4 would be Divide
 			result = float64(a / b)
 			break
 
-	case 5: //Case 5 would be Modulus
-			result = float64(a % b)
-			break
-	
 	default: //Default case
 			fmt.Println("See ya!!")
 			break
