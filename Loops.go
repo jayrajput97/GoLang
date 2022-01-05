fa075d57de687da278fa02b1c4f6968477b826b9
Jay Rajput <jayrajputd@gmail.com>
2022-01-04 19:48:36 -0500

    commit

--- Loops.go
+++ Loops.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	//This program is to run loops in GoLang
+	//For loop has 3 parts... Initialization... Condition... Increment/Decrement...
 	for i:=0; i<5; i++ {
 		fmt.Println("Loop no. ", i)
 	}
